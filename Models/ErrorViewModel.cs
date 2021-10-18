449847bea0354ada9ad433c938908613721642a6
THUY LINH NGUYEN PHUONG <1821050128@student.humg.edu.vn>
2021-10-18 09:23:20 +0700

    NPTL commit first

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcPhuongLinh.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
